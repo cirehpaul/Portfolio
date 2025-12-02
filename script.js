d3147b9b20422ad748d030b8a46ecebb6a070df8
Cire Paul Cruz <166087872+cirehpaul@users.noreply.github.com>
2025-12-02 13:18:32 +0800

    Update script.js
    Added menu toggle functionality to show/hide navigation links.

--- script.js
+++ script.js
@@ -18,5 +18,13 @@ window.addEventListener("load", function() {
     }
   });
 
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+      menuToggle.classList.toggle('open');
+    });
+
 });
     
