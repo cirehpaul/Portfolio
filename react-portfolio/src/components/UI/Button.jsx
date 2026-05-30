9d39fb3e7b8d231337cd62c39c90c87ef08b5ff0
Cire Paul Cruz <166087872+cirehpaul@users.noreply.github.com>
2026-05-30 21:12:15 +0800

    Deploy latest production build to gh-pages (#1)

--- react-portfolio/src/components/UI/Button.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function Button({children, className='', ...props}){
-  return <button className={`px-4 py-2 rounded-full ${className}`} {...props}>{children}</button>
-}
